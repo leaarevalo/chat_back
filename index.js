86088d53ee68f0a1a0977693725d02bd46d9126e
leaarevalo <leandro.arevalo@isibit.travel>
2022-08-19 09:13:20 -0300

    :sparkles: user typing added

--- index.js
+++ index.js
@@ -29,10 +29,14 @@ socketIO.on('connection', (socket) => {
         socketIO.emit('newUserResponse', users);
     })
 
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typingResponse', data);
+    })
+
 
     socket.on('disconnect', () => {
         console.log('🔥: A user disconnected');
-        users.filter((user) => user.socketID !== socket.id);
+        users = users.filter((user) => user.socketID !== socket.id);
 
         socketIO.emit('newUserResponse', users);
         socket.disconnect();
